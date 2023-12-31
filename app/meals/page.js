b00fb2a7ff9fbce614c36e25808ba972b420cc8f
Progress with the component <deMiguel@newsaktuell.de>
2023-12-31 09:19:21 +0100

    added database for meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,10 @@
 import MealsGrid from "@/components/meals/meals-grid";
+import { getAllMeals } from "@/lib/meals";
 import Link from "next/link";
 import classes from "./page.module.css";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getAllMeals();
   return (
     <>
       <header className={classes.header}>
@@ -19,7 +21,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -6,10 +6,13 @@ import classes from "./nav-link.module.css";
 
 export default function NavLink({ href, children }) {
   const path = usePathname();
+  console.log(path);
+  console.log(href);
+  console.log(path.startsWith(href) ? classes.active : undefined);
   return (
     <Link
       className={path.startsWith(href) ? classes.active : undefined}
-      href="/meals"
+      href={href}
     >
       {children}
     </Link>
