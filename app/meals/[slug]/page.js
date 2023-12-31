00a31ac3ef47a762b1392346bcd6066a374430a7
Progress with the component <deMiguel@newsaktuell.de>
2023-12-31 17:41:26 +0100

    implemented meal detail

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,31 @@
+import { getMealBySlug } from "@/lib/meals";
+import Image from "next/image";
+import classes from "./page.module.css";
+
 export default function MealDetail({ params }) {
+  const meal = getMealBySlug(params.slug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  console.log("meal.image", typeof meal.image);
   return (
-    <main>
-      <h2>Meal Detail</h2>
-      <h2>{params.slug}</h2>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator_email}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
