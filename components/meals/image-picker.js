2455fa009e0502df1c55932312cb95aa233db039
Progress with the component <deMiguel@newsaktuell.de>
2024-01-01 12:02:22 +0100

    implemented Date Picker

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,19 +1,40 @@
 "use client";
 
-import { useRef } from "react";
+import Image from "next/image";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState(null);
   const imageInput = useRef();
 
   const handlePickClick = () => {
     imageInput.current.click();
   };
 
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image src={pickedImage} fill alt="Image selected by the user" />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -21,6 +42,8 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
